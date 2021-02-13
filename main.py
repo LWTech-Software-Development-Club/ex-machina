49eddb868375e557bfb237f98ca2c4fb939cb0b4
Darren Stults <drankof@gmail.com>
2021-02-12 18:04:50 -0800

    Finished cleanup, should run as-is.

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import pandas as pd
 
-# Preparing data input from text files
-
 # ===============================================================================
-# Get the Data
+# Get the Training Data
 # ===============================================================================
 
-def getTrainingFile(fileName):
+def getSanitizedDataFromTrainingFile(fileName):
+	print("Loading Training File: " + fileName)
 	data = pd.read_csv(fileName, sep="\t", header=None)
 	data_clean = pd.DataFrame(data)
 	data_clean.columns = ['label', 'text']
@@ -14,6 +13,7 @@ def getTrainingFile(fileName):
 	return data_clean
 
 def getTestFile(fileName):
+	print("Loading Test File: " + fileName)
 	data = pd.read_csv(fileName, sep="\t", header=None)
 	data_clean = pd.DataFrame(data)
 	data_clean.columns = ['text']
@@ -21,58 +21,73 @@ def getTestFile(fileName):
 	return data_clean
 
 def sanitizeText(text):
+	print("Sanitizing text...")
 	text = text.str.replace('\W+', ' ').str.replace('\s+', ' ').str.strip()
 	text = text.str.lower()
 	text = text.str.split()
 	return text
 
-data_clean = getTrainingFile('SMSSpamCollection.short')
-# print(data_clean)
-# split and train test data
-train_data = data_clean.sample(frac=0.8, random_state=1).reset_index(drop=True)
-test_data = data_clean.drop(train_data.index).reset_index(drop=True) # drop everything from the index point which doesn't move from where it left off
-train_data = train_data.reset_index(drop=True)
+print('Commencing training file input process.')
+
+training_data = getSanitizedDataFromTrainingFile('SMSSpamCollection.short')
+# TODO: Get it to append multiple files to the same massive training data clean var
+#training_data &= getSanitizedDataFromTrainingFile('SMSSpamCollection2.short')
+
+print('Training files loaded and sanitized.')
+
+
+# Old Code:
+# print(data_clean) # Debug
+# Previously, it would split the training data into two groups, this is no longer the desired behavior.
+# train_data = data_clean.sample(frac=0.8, random_state=1).reset_index(drop=True)
+# test_data = data_clean.drop(train_data.index).reset_index(drop=True) # drop everything from the index point which doesn't move from where it left off
+# train_data = train_data.reset_index(drop=True)
 # print(train_data)
 # print(test_data)
+
 
 # ===============================================================================
 # Train the Data
 # ===============================================================================
 
+print('Training the data, this might take a while...')
+
+# Create frequency table
+vocabulary = list(set(training_data['text'].sum()))
+word_counts_per_sms = pd.DataFrame([
+	[row[1].count(word) for word in vocabulary]
+	for _, row in training_data.iterrows()], columns=vocabulary)
+training_data = pd.concat([training_data.reset_index(), word_counts_per_sms], axis=1).iloc[:,1:]
+# print(train_data.style)
+# print(tabulate(train_data, headers='keys', tablefmt='psql'))
+probabilityOfSpam = training_data['label'].value_counts()['spam'] / training_data.shape[0]
+probabilityOfHam = training_data['label'].value_counts()['ham'] / training_data.shape[0]
+numberSpam = training_data.loc[training_data['label'] == 'spam', 'text'].apply(len).sum()
+numberHam = training_data.loc[training_data['label'] == 'ham', 'text'].apply(len).sum()
+vocabularySize = len(training_data.columns) - 3
+# print(test_data)
+
+print('Data training complete.')
+
+# ===============================================================================
+# Test the Trained Data against Real Messages
+# ===============================================================================
+
 def probabilityWordIsSpam(word):
 	# Alpha — the coefficient for the cases when a word in the message is absent in our dataset.
 	alpha = 1
-	if word in train_data.columns:
-		return (train_data.loc[train_data['label'] == 'spam', word].sum() + alpha) / (numberSpam + alpha * vocabularySize)
+	if word in training_data.columns:
+		return (training_data.loc[training_data['label'] == 'spam', word].sum() + alpha) / (numberSpam + alpha * vocabularySize)
 	else:
 		return 1
 
 def probabilityWordIsHam(word):
 	# Alpha — the coefficient for the cases when a word in the message is absent in our dataset.
 	alpha = 1
-	if word in train_data.columns:
-		return (train_data.loc[train_data['label'] == 'ham', word].sum() + alpha) / (numberHam + alpha * vocabularySize)
+	if word in training_data.columns:
+		return (training_data.loc[training_data['label'] == 'ham', word].sum() + alpha) / (numberHam + alpha * vocabularySize)
 	else:
 		return 1
-
-# Make massive frequency table
-vocabulary = list(set(train_data['text'].sum()))
-word_counts_per_sms = pd.DataFrame([
-	[row[1].count(word) for word in vocabulary]
-	for _, row in train_data.iterrows()], columns=vocabulary)
-train_data = pd.concat([train_data.reset_index(), word_counts_per_sms], axis=1).iloc[:,1:]
-# print(train_data.style)
-# print(tabulate(train_data, headers='keys', tablefmt='psql'))
-probabilityOfSpam = train_data['label'].value_counts()['spam'] / train_data.shape[0]
-probabilityOfHam = train_data['label'].value_counts()['ham'] / train_data.shape[0]
-numberSpam = train_data.loc[train_data['label'] == 'spam', 'text'].apply(len).sum()
-numberHam = train_data.loc[train_data['label'] == 'ham', 'text'].apply(len).sum()
-vocabularySize = len(train_data.columns) - 3
-# print(test_data)
-
-# ===============================================================================
-# Train the Data
-# ===============================================================================
 
 def classify(message):
 	p_spam_given_message = probabilityOfSpam
@@ -90,8 +105,13 @@ def classify(message):
 def listToString(s):
 	return ' '.join(s)
 
-getTestFile('real_messages.txt')
+print('Getting real data to test against the trained data.')
 
-for _, row in data_clean.iterrows():
+test_data = getTestFile('real_messages.txt')
+
+print('Testing the trained data against the real data.')
+
+for _, row in test_data.iterrows():
 	print(classify(row['text']) + " | MSG: " + listToString(row['text']))
 
+print('Program gracefully terminated.')
